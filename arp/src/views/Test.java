8602447f53d419c27a0e7985d2a7a506b2479a8e
abajorat <ab@bajnet.de>
2014-09-28 21:34:35 -0500

    Lalala

--- arp/src/views/Test.java
+++ arp/src/views/Test.java
@@ -10,23 +10,23 @@ public class Test {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		            UIManager.setLookAndFeel(info.getClassName());
-		            break;
-		        }
-		    }
-		} catch (Exception e) {
-		    System.out.println("Error!");
-		}
-		JFrame frame = new JFrame("Test");
-		MainView view = new MainView();
-		frame.setContentPane(view);
-		frame.pack();
-		frame.setVisible(true);
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		try {
+//		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+//		        if ("Nimbus".equals(info.getName())) {
+//		            UIManager.setLookAndFeel(info.getClassName());
+//		            break;
+//		        }
+//		    }
+//		} catch (Exception e) {
+//		    System.out.println("Error!");
+//		}
+//		JFrame frame = new JFrame("Test");
+//		MainView view = new MainView();
+//		frame.setContentPane(view);
+//		frame.pack();
+//		frame.setVisible(true);
+//	}
 
 }
